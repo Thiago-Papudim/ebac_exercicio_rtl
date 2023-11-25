de42388ada88b8f32430f1bfe936c7d2a5491d2c
Thiago Soares <thiago_soares_mago@hotmail.com>
2023-11-25 14:41:36 -0300

    testes com 2 comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,25 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Post from '.';
-import PostComment from '.';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+describe('Teste para o componente', () => {
+    it('Deve renderizar corretamente', () => {
+        render(<Post />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Deve adicionar dois comentários', () => {
+        render(<Post />);
+
+        const textareaElement = screen.getByRole('textbox');
+        const submitButton = screen.getByText('Comentar');
+
+        fireEvent.change(textareaElement, { target: { value: 'Comentário 1' } });
+        fireEvent.click(submitButton);
+
+        fireEvent.change(textareaElement, { target: { value: 'Comentário 2' } });
+        fireEvent.click(submitButton);
+
+        expect(screen.getByText('Comentário 1')).toBeInTheDocument();
+        expect(screen.getByText('Comentário 2')).toBeInTheDocument();
     });
 });
